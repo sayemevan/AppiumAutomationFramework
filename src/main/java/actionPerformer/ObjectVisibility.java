ad81298bcfe1bd172f7c2ce39741a21f1518b3da
Sayem <sayem@mislbd.com>
2022-06-22 23:07:17 +0600

    ObjectVisibility class added for checking MobileElement is visible or not in our expected time.

---
+++ src/main/java/actionPerformer/ObjectVisibility.java
@@ -0,0 +1,49 @@
+package actionPerformer;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static register.DataProvider.*;
+import static register.ElementFinder.*;
+import static utilities.ObjectRepositoryActions.*;
+
+public class ObjectVisibility {
+    public static boolean visibilityAssert(String uiObjectName, String maxWaitTime, String extraParam){
+        try {
+            String uiObjectDetails = getValue(APP_LOCATOR_VALUES, APP_LOCATOR_FILE_NAME, uiObjectName);
+            if (maxWaitTime.equals("DEFAULT")) {
+                final WebDriverWait wait = new WebDriverWait(APPIUM_DRIVER, MAX_WAIT_TIME);
+                wait.until(ExpectedConditions.visibilityOfElementLocated(getSpecificElementType(uiObjectDetails)));
+            } else if (maxWaitTime.equals("NULL")) {
+                final WebDriverWait wait = new WebDriverWait(APPIUM_DRIVER, 0);
+                wait.until(ExpectedConditions.visibilityOf(getElement(uiObjectDetails)));
+            } else {
+                final WebDriverWait wait = new WebDriverWait(APPIUM_DRIVER, Integer.parseInt(maxWaitTime));
+                wait.until(ExpectedConditions.visibilityOfElementLocated(getSpecificElementType(uiObjectDetails)));
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public static boolean inVisibilityAssert(String uiObjectName, String maxWaitTime, String extraParam){
+        try {
+            String uiObjectDetails = getValue(APP_LOCATOR_VALUES, APP_LOCATOR_FILE_NAME, uiObjectName);
+            if (maxWaitTime.equals("DEFAULT")) {
+                final WebDriverWait wait = new WebDriverWait(APPIUM_DRIVER, MAX_WAIT_TIME);
+                wait.until(ExpectedConditions.invisibilityOfElementLocated(getSpecificElementType(uiObjectDetails)));
+            } else if (maxWaitTime.equals("NULL")) {
+                final WebDriverWait wait = new WebDriverWait(APPIUM_DRIVER, 0);
+                wait.until(ExpectedConditions.invisibilityOf(getElement(uiObjectDetails)));
+            } else {
+                final WebDriverWait wait = new WebDriverWait(APPIUM_DRIVER, Integer.parseInt(maxWaitTime));
+                wait.until(ExpectedConditions.invisibilityOfElementLocated(getSpecificElementType(uiObjectDetails)));
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
