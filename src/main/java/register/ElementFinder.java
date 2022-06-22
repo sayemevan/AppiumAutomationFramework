7d00cd400565b25ca6596c94b3c625393f8d7dbd
Sayem <sayem@mislbd.com>
2022-06-22 23:09:03 +0600

    ElementFinder class added to find MobileElements more dynamically.

---
+++ src/main/java/register/ElementFinder.java
@@ -0,0 +1,31 @@
+package register;
+
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.By;
+
+import static register.DataProvider.*;
+
+public class ElementFinder {
+    public static MobileElement getElement(String searchedElementString) {
+        MobileElement getElement = APPIUM_DRIVER.findElement(getSpecificElementType(searchedElementString));
+        if (getElement != null) {
+            return getElement;
+        } else {
+            return null;
+        }
+    }
+    public static By getSpecificElementType(String searchedElementString){
+        String[] splitedElementString = searchedElementString.split("~");
+        if(splitedElementString[0].trim().equals("ID")){
+            return By.id(splitedElementString[1]);
+        } else if(splitedElementString[0].trim().equals("XPATH")){
+            return By.xpath(splitedElementString[1]);
+        } else if(splitedElementString[0].trim().equals("NAME")){
+            return By.name(splitedElementString[1]);
+        } else if(splitedElementString[0].trim().equals("CLASSNAME")){
+            return By.className(splitedElementString[1]);
+        } else {
+            return null;
+        }
+    }
+}
