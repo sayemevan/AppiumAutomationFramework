7c7f5dcb2e0b587724d5d05d8f5b89a8fa14c31c
Sayem <sayem@mislbd.com>
2022-06-22 23:06:29 +0600

    propertyFileReader class added for reading and updating property data.

--- src/main/java/dataProvider/propertyFileReader.java
+++ src/main/java/dataProvider/propertyFileReader.java
@@ -1,4 +1,21 @@
 package dataProvider;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 public class propertyFileReader {
+
+    public static Properties objectRepositoryGet(String propertyFilePath, String propertyFileName) {
+        try {
+            InputStream stream;
+            stream = ClassLoader.getSystemResourceAsStream(propertyFilePath + propertyFileName + ".properties");
+            Properties properties = new Properties();
+            properties.load(stream);
+            return properties;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
