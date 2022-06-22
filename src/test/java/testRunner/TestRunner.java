26fea328833d8685e674a501df9d696984944aa6
Sayem <sayem@mislbd.com>
2022-06-22 16:19:33 +0600

    TestRunner class updated.

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -5,9 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/functionalTest", glue = {"stepDefinitions"},
-        plugin = {"pretty", "junit:target/cucumber-reports/Cucumber.xml", "html:target/cucumber-reports",
-                "json:target/cucumber-reports/Cucumber.json"})
+@CucumberOptions(features = "src/test/resources/functionalTest", glue = {"stepDefinitions"})
 public class TestRunner {
 
 }
