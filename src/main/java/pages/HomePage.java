d7014316647765af7b2e252a878fd7583f5b3b7d
Sayem <sayem@mislbd.com>
2022-06-23 18:58:29 +0600

    HomePage functionality added.

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,5 +1,31 @@
 package pages;
 
+import actionPerformer.AppSpecificActions;
+
+import static actionPerformer.ObjectSpecificActions.actionSet;
+import static actionPerformer.ObjectVisibility.visibilityAssert;
+import static utilities.SpecificItemSelector.dynamicScrollToElement;
+
 public class HomePage {
+
+    public static void userOnHomePage(){
+        try {
+            AppSpecificActions.lunchApp();
+            actionSet("UiReadAndAccept", "LCLICK", null, null);
+            Thread.sleep(3000);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void selectCategoryFromHomePage(String categoryName) {
+        try {
+            visibilityAssert("UiOurCategories", "DEFAULT", null);
+            dynamicScrollToElement("UiSpecificCategoriesList", "UiAllCategoriesList", categoryName);
+            Thread.sleep(3000);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
 }
