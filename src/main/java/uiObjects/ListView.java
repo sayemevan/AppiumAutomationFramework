ae4926c1043ddc0d4b8ea4bb838fa4c1582a6e0f
Sayem <sayem@mislbd.com>
2022-06-23 18:56:20 +0600

    ListView new object type added.

---
+++ src/main/java/uiObjects/ListView.java
@@ -0,0 +1,9 @@
+package uiObjects;
+
+import io.appium.java_client.MobileElement;
+
+public class ListView extends GenericTypes{
+    public ListView(MobileElement wrappedMobileElement) {
+        super(wrappedMobileElement);
+    }
+}
