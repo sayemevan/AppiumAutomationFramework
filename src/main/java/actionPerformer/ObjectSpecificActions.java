7ba9907f86166c1032d2b68796a77fe25be07426
Sayem <sayem@mislbd.com>
2022-06-23 19:00:01 +0600

    New object specific methods are added, groupActionSet, valueGet.

--- src/main/java/actionPerformer/ObjectSpecificActions.java
+++ src/main/java/actionPerformer/ObjectSpecificActions.java
@@ -1,17 +1,22 @@
 package actionPerformer;
 
 import io.appium.java_client.MobileElement;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.Select;
 import uiObjects.*;
 
 import static actionPerformer.ObjectVisibility.*;
+import static dataProvider.ExcelUtilities.excelSheetDataGet;
 import static register.DataProvider.*;
 import static register.ElementFinder.*;
 import static utilities.ObjectRepositoryActions.*;
+import static utilities.SpecificItemSelector.*;
 
 public class ObjectSpecificActions {
+
     public static boolean actionSet(String uiObjectName, String actionType, String valueToBeSet, String extraParam){
         try {
-            String uiObjectDetails = getValue(APP_LOCATOR_VALUES, APP_LOCATOR_FILE_NAME, uiObjectName);
+            String uiObjectDetails = getRepoValue(uiObjectName);
             if (!visibilityAssert(uiObjectName, "DEFAULT", null)) {
                 return false;
             }
@@ -19,6 +24,11 @@ public class ObjectSpecificActions {
             if (uiObjectName == null || actionType == null || mobileElement == null) {
                 return false;
             }
+            if(valueToBeSet == null){
+                valueToBeSet = "NULL";
+            }
+            scrollUntilElementVisible(uiObjectName, null);
+
             switch (uiObjectDetails.split("~")[2].trim().toUpperCase()) {
                 case "TEXTBOX":
                     TextBox textBox = new TextBox(mobileElement);
@@ -26,8 +36,14 @@ public class ObjectSpecificActions {
                         case "DEFAULT":
                         case "VALUESET":
                         case "TEXTINPUT":
+                            if (valueToBeSet.equals("BLANK") || valueToBeSet.equals("NULL")) {
+                                textBox.sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+                            } else {
+                                textBox.sendKeys(valueToBeSet);
+                            }
                             break;
                         case "SETBLANK":
+                            textBox.sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
                             break;
                         case "LCLICK":
                             textBox.click();
@@ -70,6 +86,13 @@ public class ObjectSpecificActions {
                         case "DEFAULT":
                         case "VALUESET":
                         case "ITEMSELECT":
+//                            Select select = new Select(listBox);
+//                            select.selectByValue("Bangladesh");
+                            listBox.click();
+                            //For selecting Bangladesh only
+                            Thread.sleep(1000);
+                            getElement(getRepoValue("UiBangladesh")).click();
+                            Thread.sleep(2000);
                             break;
                         case "ITEMDESELECT":
                         case "SETBLANK":
@@ -123,9 +146,45 @@ public class ObjectSpecificActions {
                             break;
                     }
                     break;
+                case "LISTVIEW":
+                    ListView listView = new ListView(mobileElement);
+                    switch (actionType.trim().toUpperCase()) {
+                        case "DEFAULT":
+                        case "LCLICK":
+                            listView.click();
+                            break;
+                        case "RCLICK":
+                            break;
+                        case "MOUSEOVER":
+                            break;
+                    }
+                    break;
             }
             return true;
         } catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean groupDatActionSetFromExcel(String dataSheetName, String extraParam) {
+        try {
+            String uiObjectName, actionType, valueToBeSet;
+            if(excelSheetDataGet(EXCEL_SHEET_FILE_NAME, EXCEL_SHEET_FILE_PATH, dataSheetName, null)) {
+                for (int j = 0; j < excelSheetList.get(dataSheetName).size(); j++) {
+                    uiObjectName = excelSheetList.get(dataSheetName).get(j).get("UiObjectName");
+                    actionType = excelSheetList.get(dataSheetName).get(j).get("ActionType");
+                    valueToBeSet = excelSheetList.get(dataSheetName).get(j).get("ValueToBeSet");
+                    if(valueToBeSet.startsWith("\"") && valueToBeSet.endsWith("\"")){
+                        valueToBeSet = valueToBeSet.substring(1, valueToBeSet.length()-1);
+                    }
+
+                    scrollUntilElementVisible(uiObjectName, null);
+                    actionSet(uiObjectName, actionType, valueToBeSet, extraParam);
+                }
+            }
+            return true;
+        } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
--- src/main/java/actionPerformer/ObjectSpecificValues.java
+++ src/main/java/actionPerformer/ObjectSpecificValues.java
@@ -1,4 +1,90 @@
 package actionPerformer;
 
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.Keys;
+import uiObjects.*;
+
+import static actionPerformer.ObjectVisibility.visibilityAssert;
+import static register.ElementFinder.getElement;
+import static utilities.ObjectRepositoryActions.getRepoValue;
+import static utilities.SpecificItemSelector.scrollUntilElementVisible;
+
 public class ObjectSpecificValues {
+    public static String valueGet(String uiObjectName, String propertyName, String extraParam){
+        try {
+            String outputValue = "", uiObjectDetails = getRepoValue(uiObjectName);
+            if (!visibilityAssert(uiObjectName, "DEFAULT", null)) {
+                return outputValue;
+            }
+            MobileElement mobileElement = getElement(uiObjectDetails);
+            if (uiObjectName == null || propertyName == null || mobileElement == null || propertyName == null) {
+                return outputValue;
+            }
+
+            scrollUntilElementVisible(uiObjectName, null);
+
+            switch (uiObjectDetails.split("~")[2].trim().toUpperCase()) {
+                case "TEXTBOX":
+                    TextBox textBox = new TextBox(mobileElement);
+                    outputValue = propertyNameExtractor(propertyName, textBox);
+                    break;
+                case "PUSHBUTTON":
+                    PushButton pushButton = new PushButton(mobileElement);
+                    outputValue = propertyNameExtractor(propertyName, pushButton);
+                    break;
+                case "ICONBUTTON":
+                    IconButton iconButton = new IconButton(mobileElement);
+                    outputValue = propertyNameExtractor(propertyName, iconButton);
+                    break;
+                case "LISTBOX":
+                    ListBox listBox = new ListBox(mobileElement);
+                    outputValue = propertyNameExtractor(propertyName, listBox);
+                    break;
+                case "PREVIEW":
+                    PreView preView = new PreView(mobileElement);
+                    outputValue = propertyNameExtractor(propertyName, preView);
+                    break;
+                case "RADIOBUTTON":
+                    RadioButton radioButton = new RadioButton(mobileElement);
+                    outputValue = propertyNameExtractor(propertyName, radioButton);
+                    break;
+                case "LISTVIEW":
+                    ListView listView = new ListView(mobileElement);
+                    outputValue = propertyNameExtractor(propertyName, listView);
+                    break;
+            }
+            return outputValue;
+        } catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static String propertyNameExtractor(String propertyName, MobileElement mobileElement) {
+        String outputValue = "";
+        switch (propertyName.trim().toUpperCase()) {
+            case "DEFAULT":
+            case "CURRTEXT":
+            case "CURRVAL":
+                outputValue = mobileElement.getText();
+                break;
+            case "OBJWIDTH":
+                outputValue = String.valueOf(mobileElement.getSize().getWidth());
+                break;
+            case "OBJHEIGHT":
+                outputValue = String.valueOf(mobileElement.getSize().getHeight());
+                break;
+            case "OBJLABEL":
+                break;
+            case "XCOORD":
+                outputValue = String.valueOf(mobileElement.getLocation().getX());
+                break;
+            case "YCOORD":
+                outputValue = String.valueOf(mobileElement.getLocation().getY());
+                break;
+            case "WRNLABEL":
+                break;
+        }
+        return outputValue;
+    }
 }
